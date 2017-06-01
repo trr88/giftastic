a261dee7d00b289e25bef9df14c9aa1caafeb5e8
Tom Renicker <trrenicker@gmail.com>
2017-06-01 18:26:20 -0400

    added 's' to http

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,7 +5,7 @@ window.onload = function() {
     function displayGif() {
 
         var theme = $(this).attr("data-name");
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + theme + "&limit=" + limit + "&api_key=dc6zaTOxFJmzC";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + theme + "&limit=" + limit + "&api_key=dc6zaTOxFJmzC";
 
         //     // Creates AJAX call for the specific movie button being clicked
         $.ajax({
